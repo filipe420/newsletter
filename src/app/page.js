9098ffd8a0efcb760577e454e22063053d9b8f62
filipe420 <145150193+filipe420@users.noreply.github.com>
2024-06-26 01:29:48 -0300

    01comit

---
+++ src/app/page.js
@@ -0,0 +1,22 @@
+
+import Image from "next/image";
+import 'react-toastify/dist/ReactToastify.css';
+import FormEmail from "@/componetes/FormEmail";
+export default function Home() {
+
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <div className="h-[28rem] p-5 w-2/3 bg-black rounded-lg flex">
+        <div className="relative flex justify-center items-center bg-indigo-500 flex-[2] min-h-full rounded">
+          <Image src="https://cdn-icons-png.flaticon.com/512/4939/4939994.png" alt="hero" layout="fill" objectFit="contain" className="rounded max-h-36 flex mt-20" />
+        </div>
+        <div className="flex-[3] px-16 min-h-full flex flex-col text-white justify-center items-center gap-5">
+          <h1 className="text-2xl">Se inscreva para receber novidades</h1>
+          <p className="text-lg">Nunca mais se esqueça de comprar seus insumos semanais com qualidade de vida!</p>
+          <FormEmail />
+        </div>
+      </div>
+    </main>
+  );
+}
